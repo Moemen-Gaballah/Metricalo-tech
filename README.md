0ce66edb517ee0ba70e3e26c44817940bcdfbdd2
Moemen Gaballa <moemengaballa@gmail.com>
2025-03-01 14:55:34 +0200

    update payment

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Metricalo-tech-
+# Metricalo-tech
+
+# update file docker file
+
+# update docker compose and check it
+
+## build and start
+`docker-compose up -d --build`
+
+# end point : http://127.0.0.1:8000/app/example/shift4
+# end point : http://127.0.0.1:8000/app/example/shift4
--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -6,9 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use App\Service\PaymentServiceInterface;
+use App\Service\Shift4Service;
+use App\Service\AciService;
+use Symfony\Component\Routing\Attribute\Route;
 
 class PaymentController extends AbstractController
 {
+    private Shift4Service $shift4Service;
+    private AciService $aciService;
+
+    public function __construct(Shift4Service $shift4Service, AciService $aciService)
+    {
+        $this->shift4Service = $shift4Service;
+        $this->aciService = $aciService;
+    }
+
+    #[Route('/app/example/{provider}', name: 'payment', methods: ['POST'])]
     public function purchase(string $provider, Request $request): JsonResponse
     {
         $service = $this->resolveService($provider);
@@ -27,9 +40,9 @@ class PaymentController extends AbstractController
 
     private function resolveService(string $provider): ?PaymentServiceInterface
     {
-        return match($provider) {
-            'shift4' => $this->container->get('App\Service\Shift4Service'),
-            'aci' => $this->container->get('App\Service\AciService'),
+        return match ($provider) {
+            'shift4' => $this->shift4Service,
+            'aci' => $this->aciService,
             default => null,
         };
     }
